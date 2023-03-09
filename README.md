560a80c0d52323a71eacd5a5816e550d6a2602d6
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-09 14:31:54 +0530

    sidebar complete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Project-YT-clone
+
+This is a follow along project, not self made.
+
+Ive done this to learn React by absorbing things done by professional coders
+through their techniques and processes.
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,6 +21,23 @@ const Feed = () => {
 					Copyright 2022 Rz
 				</Typography>
 			</Box>
+			<Box
+				p={2}
+				sx={{
+					overflowY: "auto",
+					height: "90vh",
+					flex: 2,
+				}}
+			>
+				<Typography
+					variant="h4"
+					fontWeight="bold"
+					mb={2}
+					sx={{ color: "white" }}
+				>
+					New<span style={{ color: "#F31503" }}>video</span>
+				</Typography>
+			</Box>
 		</Stack>
 	);
 };
